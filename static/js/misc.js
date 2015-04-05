eec42b69a6013db3aeddc3ff36779d73db41f323
Karl Woditsch <k.woditsch@gmail.com>
2015-04-05 16:30:34 +0200

    Fix #2

--- static/js/misc.js
+++ static/js/misc.js
@@ -1,20 +1,43 @@
 /* Sidebar */
 
 var sidebar = $("#sidebar");
-sidebar.affix({
-  offset: {
-    top: 50,
-    bottom: function () {
-      return (this.bottom = $('.footer').outerHeight(true))
-    }
+var main = $("#main");
+
+function sidebarAffixRefresh(){
+  if(sidebar && main && (main.innerHeight() + 50 > sidebar.innerHeight())){
+    sidebar.affix({
+      offset: {
+        top: 50,
+        bottom: function () {
+          return (this.bottom = $('.footer').outerHeight(true) + 20)
+        }
+      }
+    });
   }
-});
+}
+
+if(sidebar){
+  sidebar.on('affixed.bs.affix', function () {
+    sidebar.css("position", "");
+  });
+  
+  $(document).ready(function() {
+    sidebarAffixRefresh();
+    
+    $('.collapse').on('shown.bs.collapse', function () {
+      sidebarAffixRefresh();
+    });
+    $('.collapse').on('hidden.bs.collapse', function () {
+      sidebarAffixRefresh();
+    });
+  });
+}
 
 $(".collapse-all").on('click', function(e) {
   e.preventDefault();
   $(".collapse").collapse('hide');
 });
-
+  
 /* Anchors */
 
 $("a").on('click', function(e) {
